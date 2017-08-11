4b6ca3e61d89495c0b3d28f1e38484100438441f
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-08-11 01:18:32 -0500

    adding charts.
    adding charts features.

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -16,6 +16,7 @@
             'ui.bootstrap.datetimepicker',
             'ui.router',
             'infinite-scroll',
+            'chart.js',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
             'angular-loading-bar'
         ])
