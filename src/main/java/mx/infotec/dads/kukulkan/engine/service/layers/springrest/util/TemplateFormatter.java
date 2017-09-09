a06fdc2e9c9c4487ab50f53c9dbbc2ca220b10c0
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-09-08 20:41:37 -0500

    updating values
    updating values

--- src/main/java/mx/infotec/dads/kukulkan/engine/service/layers/springrest/util/TemplateFormatter.java
+++ src/main/java/mx/infotec/dads/kukulkan/engine/service/layers/springrest/util/TemplateFormatter.java
@@ -13,20 +13,4 @@ public class TemplateFormatter {
     public static String formatNameTemplate(String template) {
     	return template.replaceAll("entity|.ftl", "");
     }
-
-    public static void main(String[] args) {
-        System.out.println(formatNameTemplate("entity.controller.js.ftl"));
-        System.out.println(formatNameTemplate("entity-delete-dialog.controller.js.ftl"));
-        System.out.println(formatNameTemplate("entity-delete-dialog.html.ftl"));
-        System.out.println(formatNameTemplate("entity-detail.controller.js.ftl"));
-        System.out.println(formatNameTemplate("entity-detail.html.ftl"));
-        System.out.println(formatNameTemplate("entity-dialog.controller.js.ftl"));
-        System.out.println(formatNameTemplate("entity-dialog.html.ftl"));
-        System.out.println(formatNameTemplate("entity.html.ftl"));
-        System.out.println(formatNameTemplate("entity.search.service.js.ftl"));
-        System.out.println(formatNameTemplate("entity.service.js.ftl"));
-        System.out.println(formatNameTemplate("entity.state.js.ftl"));
-        System.out.println(formatNameTemplate("entity.js.ftl"));
-        System.out.println(formatNameTemplate("entity-detail.html.ftl"));
-    }
 }
