98f77690ba336c98a3257d766387b31faf58cc80
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-10-19 17:56:26 -0500

    refactoring DataModelElement
    refactoring the data model element

--- src/main/java/mx/infotec/dads/kukulkan/engine/domain/core/DataModelElement.java
+++ src/main/java/mx/infotec/dads/kukulkan/engine/domain/core/DataModelElement.java
@@ -36,243 +36,243 @@ import java.util.TreeSet;
  */
 public class DataModelElement {
 
-	/**
-	 * id of the dataModelElement, it is the primary key
-	 */
-	protected PrimaryKey primaryKey;
+    /**
+     * id of the dataModelElement, it is the primary key
+     */
+    protected PrimaryKey primaryKey;
 
-	/**
-	 * urlName statements of the elements
-	 */
-	protected String urlName;
+    /**
+     * urlName statements of the elements
+     */
+    protected String urlName;
 
-	/**
-	 * imports statements of the elements
-	 */
-	protected Collection<String> imports;
+    /**
+     * imports statements of the elements
+     */
+    protected Collection<String> imports;
 
-	/**
-	 * Name of the dataModel, usually it is the name of the Schema formatted for
-	 * specific technology. For instance, in java it is the java Class name
-	 */
-	protected String name;
+    /**
+     * Name of the dataModel, usually it is the name of the Schema formatted for
+     * specific technology. For instance, in java it is the java Class name
+     */
+    protected String name;
 
-	/**
-	 * The name of the database Table
-	 */
-	protected String tableName;
+    /**
+     * The name of the database Table
+     */
+    protected String tableName;
 
-	/**
-	 * The name of the table formated to java world.
-	 */
-	protected String camelCaseFormat;
+    /**
+     * The name of the table formated to java world.
+     */
+    protected String camelCaseFormat;
 
-	/**
-	 * The name of the table formated to java world.
-	 */
-	protected String camelCasePluralFormat;
+    /**
+     * The name of the table formated to java world.
+     */
+    protected String camelCasePluralFormat;
 
-	/**
-	 * The columns mapped into the PropertyHolder class
-	 */
-	protected String qualifiedName;
+    /**
+     * The columns mapped into the PropertyHolder class
+     */
+    protected String qualifiedName;
 
-	/**
-	 * The columns mapped into the PropertyHolder class
-	 */
-	protected Collection<Property> properties;
+    /**
+     * The columns mapped into the PropertyHolder class
+     */
+    protected Collection<Property> properties;
 
-	/**
-	 * The generated elements
-	 */
-	protected List<GeneratedElement> generatedElements = new ArrayList<>();
+    /**
+     * The generated elements
+     */
+    protected List<GeneratedElement> generatedElements = new ArrayList<>();
 
-	protected boolean hasNotNullElements;
+    protected boolean hasNotNullElements;
 
-	protected boolean hasTimeProperties;
+    protected boolean hasTimeProperties;
 
-	protected boolean hasBlobProperties;
+    protected boolean hasBlobProperties;
 
-	protected boolean hasClobProperties;
+    protected boolean hasClobProperties;
 
-	protected boolean hasConstraints;
+    protected boolean hasConstraints;
 
-	protected boolean hasBigDecimal;
+    protected boolean hasBigDecimal;
 
-	protected boolean hasInstant;
+    protected boolean hasInstant;
 
-	protected boolean hasLocalDate;
+    protected boolean hasLocalDate;
 
-	protected boolean hasZoneDateTime;
+    protected boolean hasZoneDateTime;
 
-	public boolean isHasConstraints() {
-		return hasConstraints;
-	}
+    public boolean isHasConstraints() {
+        return hasConstraints;
+    }
 
-	public void setHasConstraints(boolean hasConstraints) {
-		this.hasConstraints = hasConstraints;
-	}
+    public void setHasConstraints(boolean hasConstraints) {
+        this.hasConstraints = hasConstraints;
+    }
 
-	public boolean isHasBigDecimal() {
-		return hasBigDecimal;
-	}
+    public boolean isHasBigDecimal() {
+        return hasBigDecimal;
+    }
 
-	public void setHasBigDecimal(boolean hasBigDecimal) {
-		this.hasBigDecimal = hasBigDecimal;
-	}
+    public void setHasBigDecimal(boolean hasBigDecimal) {
+        this.hasBigDecimal = hasBigDecimal;
+    }
 
-	public boolean isHasInstant() {
-		return hasInstant;
-	}
+    public boolean isHasInstant() {
+        return hasInstant;
+    }
 
-	public void setHasInstant(boolean hasInstant) {
-		this.hasInstant = hasInstant;
-	}
+    public void setHasInstant(boolean hasInstant) {
+        this.hasInstant = hasInstant;
+    }
 
-	public boolean isHasLocalDate() {
-		return hasLocalDate;
-	}
+    public boolean isHasLocalDate() {
+        return hasLocalDate;
+    }
 
-	public void setHasLocalDate(boolean hasLocalDate) {
-		this.hasLocalDate = hasLocalDate;
-	}
+    public void setHasLocalDate(boolean hasLocalDate) {
+        this.hasLocalDate = hasLocalDate;
+    }
 
-	public boolean isHasZoneDateTime() {
-		return hasZoneDateTime;
-	}
+    public boolean isHasZoneDateTime() {
+        return hasZoneDateTime;
+    }
 
-	public void setHasZoneDateTime(boolean hasZoneDateTime) {
-		this.hasZoneDateTime = hasZoneDateTime;
-	}
+    public void setHasZoneDateTime(boolean hasZoneDateTime) {
+        this.hasZoneDateTime = hasZoneDateTime;
+    }
 
-	private DataModelElement() {
+    private DataModelElement() {
 
-	}
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Collection<Property> getProperties() {
-		return properties;
-	}
+    public Collection<Property> getProperties() {
+        return properties;
+    }
 
-	public void setProperties(Collection<Property> properties) {
-		this.properties = properties;
-	}
+    public void setProperties(Collection<Property> properties) {
+        this.properties = properties;
+    }
 
-	public String getTableName() {
-		return tableName;
-	}
+    public String getTableName() {
+        return tableName;
+    }
 
-	public void setTableName(String tableName) {
-		this.tableName = tableName;
-	}
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
 
-	public String getCamelCaseFormat() {
-		return camelCaseFormat;
-	}
+    public String getCamelCaseFormat() {
+        return camelCaseFormat;
+    }
 
-	public void setCamelCaseFormat(String camelCaseFormat) {
-		this.camelCaseFormat = camelCaseFormat;
-	}
+    public void setCamelCaseFormat(String camelCaseFormat) {
+        this.camelCaseFormat = camelCaseFormat;
+    }
 
-	public String getUrlName() {
-		return urlName;
-	}
+    public String getUrlName() {
+        return urlName;
+    }
 
-	public void setUrlName(String urlName) {
-		this.urlName = urlName;
-	}
+    public void setUrlName(String urlName) {
+        this.urlName = urlName;
+    }
 
-	public Collection<String> getImports() {
-		return imports;
-	}
+    public Collection<String> getImports() {
+        return imports;
+    }
 
-	public void setImports(Collection<String> imports) {
-		this.imports = imports;
-	}
+    public void setImports(Collection<String> imports) {
+        this.imports = imports;
+    }
 
-	public PrimaryKey getPrimaryKey() {
-		return primaryKey;
-	}
+    public PrimaryKey getPrimaryKey() {
+        return primaryKey;
+    }
 
-	public void setPrimaryKey(PrimaryKey primaryKey) {
-		this.primaryKey = primaryKey;
-	}
+    public void setPrimaryKey(PrimaryKey primaryKey) {
+        this.primaryKey = primaryKey;
+    }
 
-	public void addProperty(Property propertyHolder) {
-		properties.add(propertyHolder);
-	}
+    public void addProperty(Property propertyHolder) {
+        properties.add(propertyHolder);
+    }
 
-	public static DataModelElement createOrderedDataModel() {
-		DataModelElement dme = new DataModelElement();
-		dme.setImports(new TreeSet<>());
-		dme.setProperties(new TreeSet<>());
-		return dme;
-	}
+    public static DataModelElement createOrderedDataModel() {
+        DataModelElement dme = new DataModelElement();
+        dme.setImports(new TreeSet<>());
+        dme.setProperties(new TreeSet<>());
+        return dme;
+    }
 
-	public String getQualifiedName() {
-		return qualifiedName;
-	}
+    public String getQualifiedName() {
+        return qualifiedName;
+    }
 
-	public void setQualifiedName(String qualifiedName) {
-		this.qualifiedName = qualifiedName;
-	}
+    public void setQualifiedName(String qualifiedName) {
+        this.qualifiedName = qualifiedName;
+    }
 
-	public boolean isHasNotNullElements() {
-		return hasNotNullElements;
-	}
+    public boolean isHasNotNullElements() {
+        return hasNotNullElements;
+    }
 
-	public void setHasNotNullElements(boolean hasNotNullElements) {
-		this.hasNotNullElements = hasNotNullElements;
-	}
+    public void setHasNotNullElements(boolean hasNotNullElements) {
+        this.hasNotNullElements = hasNotNullElements;
+    }
 
-	public boolean isHasTimeProperties() {
-		return hasTimeProperties;
-	}
+    public boolean isHasTimeProperties() {
+        return hasTimeProperties;
+    }
 
-	public void setHasTimeProperties(boolean hasTimeProperties) {
-		this.hasTimeProperties = hasTimeProperties;
-	}
+    public void setHasTimeProperties(boolean hasTimeProperties) {
+        this.hasTimeProperties = hasTimeProperties;
+    }
 
-	public boolean isHasBlobProperties() {
-		return hasBlobProperties;
-	}
+    public boolean isHasBlobProperties() {
+        return hasBlobProperties;
+    }
 
-	public void setHasBlobProperties(boolean hasBlobProperties) {
-		this.hasBlobProperties = hasBlobProperties;
-	}
+    public void setHasBlobProperties(boolean hasBlobProperties) {
+        this.hasBlobProperties = hasBlobProperties;
+    }
 
-	public void setHasClobProperties(boolean hasClobProperties) {
-		this.hasClobProperties = hasClobProperties;
-	}
+    public void setHasClobProperties(boolean hasClobProperties) {
+        this.hasClobProperties = hasClobProperties;
+    }
 
-	public boolean isHasClobProperties() {
-		return hasClobProperties;
-	}
+    public boolean isHasClobProperties() {
+        return hasClobProperties;
+    }
 
-	public String getCamelCasePluralFormat() {
-		return camelCasePluralFormat;
-	}
+    public String getCamelCasePluralFormat() {
+        return camelCasePluralFormat;
+    }
 
-	public void setCamelCasePluralFormat(String camelCasePluralFormat) {
-		this.camelCasePluralFormat = camelCasePluralFormat;
-	}
+    public void setCamelCasePluralFormat(String camelCasePluralFormat) {
+        this.camelCasePluralFormat = camelCasePluralFormat;
+    }
 
-	public List<GeneratedElement> getGeneratedElements() {
-		return generatedElements;
-	}
+    public List<GeneratedElement> getGeneratedElements() {
+        return generatedElements;
+    }
 
-	public void setGeneratedElements(List<GeneratedElement> generatedElements) {
-		this.generatedElements = generatedElements;
-	}
-	
-	public void addGeneratedElement(GeneratedElement element){
-		this.generatedElements.add(element);
-	}
+    public void setGeneratedElements(List<GeneratedElement> generatedElements) {
+        this.generatedElements = generatedElements;
+    }
+
+    public void addGeneratedElement(GeneratedElement element) {
+        this.generatedElements.add(element);
+    }
 }
