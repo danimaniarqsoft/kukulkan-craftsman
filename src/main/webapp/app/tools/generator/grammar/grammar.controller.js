ad6f67bace37e39da33d15e4f7083dfa2225ba09
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-11-30 21:16:02 -0600

    refactoring css
    refactoring css

--- src/main/webapp/app/tools/generator/grammar/grammar.controller.js
+++ src/main/webapp/app/tools/generator/grammar/grammar.controller.js
@@ -17,31 +17,40 @@
             "\tnombre String required,\n"+
             "\tedad Integer,\n"+
             "\tnumeroCredencial Long,\n"+
-            "\tsueldo BigDecimal,\n"+
-            "\timpuesto Float,\n"+
-            "\timpuestoDetalle Double,\n"+
-            "\tactivo Boolean,\n"+
             "\tfechaLocalDate LocalDate,\n"+
             "\tfechaZoneDateTime ZonedDateTime,\n"+
             "\timagen Blob,\n"+
-            "\timagenAnyBlob AnyBlob,\n"+
-            "\timagenBlob ImageBlob,\n"+
-            "\tdesc TextBlob,\n"+
-            "\tinstante Instant\n"+
+            "\tdesc TextBlob\n"+
             "}";
-        
+        vm.console ="kukulkan >\n";
+        vm.aceLoaded  = function (_editor) {
+        	// Options
+        	_editor.setReadOnly(false);
+        	_editor.setFontSize(20);
+        };
+        vm.consoleLoaded  = function (_editor) {
+        	// Options
+        	_editor.setOptions({
+        	    readOnly : true,
+        	    fontSize: 20
+        	})
+        };
         vm.editorOptions = {
+    			useWrapMode : true,
+    			showGutter: true,
+    			mode: 'java',
+    			firstLineNumber: 1,
+    			onLoad: vm.aceLoaded
+    			};
+        vm.consoleOptions = {
     			useWrapMode : true,
     			showGutter: true,
     			mode: 'java',
     			theme:'twilight',
     			firstLineNumber: 1,
-    			onLoad: vm.aceLoaded
+    			onLoad: vm.consoleLoaded
     			};
-    		vm.aceLoaded  = function (_editor) {
-    			// Options
-    		    _editor.setReadOnly(false);
-    		};
+        
 
     	var unsubscribe = $rootScope.$on('kukulkancraftsmanApp:grammarUpdate', function(event, result) {
     			vm.generatedDto = result;
@@ -50,14 +59,19 @@
         $scope.$on('$destroy', unsubscribe);
         
         function action(option){
+        	vm.console = vm.console+">Generating app...\n";
+        	vm.console = vm.console+">Connection to the server...\n";
         	Grammar.generateCode(vm.code, onConnectionSuccess, onConnectionError);
+        	vm.console = vm.console+">App Generation in process, please wait...\n";
         }
         
         function onConnectionSuccess (result) {
+        	vm.console = vm.console+">Success, app generated :)\n";
         	$scope.$emit('kukulkancraftsmanApp:grammarUpdate', result);
         }
         
         function onConnectionError () {
+        	vm.console = vm.console+">Error, please check the console:(\n";
         }
     }
 })();
